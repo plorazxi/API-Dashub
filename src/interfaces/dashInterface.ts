d694b27b23dd8ac201b37dc800b8813dc7e4e65d
plorazxi <paulo.ss.loraschi@gmail.com>
2024-10-23 20:57:18 -0300

    função create dashboard

--- src/interfaces/dashInterface.ts
+++ src/interfaces/dashInterface.ts
@@ -5,4 +5,15 @@ export const user_tokenSchema = z.object({
     email: z.string().email(),
     nome: z.string(),
     senha: z.string()
-})
+});
+
+export const ReqNewDashSchema = z.object({
+    nome: z.string(),
+    token: z.string()
+});
+
+export const NewDashSchema = z.object({
+    nome: z.string(),
+    userId: z.string(),
+    user: user_tokenSchema
+});
--- src/routes/dash.ts
+++ src/routes/dash.ts
@@ -2,7 +2,8 @@ import { PrismaClient } from "@prisma/client";
 import express from "express";
 import { verify } from "jsonwebtoken";
 import { ENV } from "../env";
-import { user_tokenSchema } from "../interfaces/dashInterface";
+import { ReqNewDashSchema, user_tokenSchema } from "../interfaces/dashInterface";
+import { connect } from "http2";
 
 const router = express.Router();
 const prisma = new PrismaClient;
@@ -25,6 +26,27 @@ router.get('/:token', async (req, res) => {
         },
     });
     res.send(dashboards);
+});
+
+router.post('/create', async (req, res) => {
+    const request = ReqNewDashSchema.parse(req.body);
+    let ver_token: object | null | void = verify(request.token, ENV.SECRETKEY, (err, decoded) => {
+        if (err) {
+            res.status(401).send({ msg: "token inválido" });
+            return null;
+        } else return decoded;
+    });
+    if(ver_token === null) {
+        return ;
+    }
+    let user = user_tokenSchema.parse(ver_token);
+    await prisma.dashboard.create({
+        data: {
+            nome: request.nome,
+            userid: user.id,
+        }
+    });
+    res.send({msg: "Dashboard criado com sucesso"});
 });
 
 module.exports = router;
